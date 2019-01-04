b3c53fde9af74dab815b0fb69eec9f5158021dd9
sgoyal2 <sgoyal2@ms.ds.uhc.com>
2019-01-04 13:38:48 +0530

    first commit Eclipse

---
+++ src/test/java/GoogleTestAutomation.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class GoogleTestAutomation {
+
+	public static WebDriver driver;
+
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sgoyal2\\Desktop\\chromedriver.exe");
+		
+		ChromeOptions options =new ChromeOptions();
+		options.setExperimentalOption("useAutomationExtension", false);
+		
+		driver = new ChromeDriver(options);
+		driver.get("https://www.google.com/webhp");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("sandeep");;
+		driver.findElement(By.name("btnK")).submit();
+		//driver.close();
+		//driver.quit();
+		
+	}
+}
